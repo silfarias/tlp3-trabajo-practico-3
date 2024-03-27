753bf80f324d2329498eca5bb4e6f9e5988bbad9
Silvana Farias <silvifarias17@gmail.com>
2024-03-27 15:26:55 -0300

    funcion missing modif

--- missing-number.py
+++ missing-number.py
@@ -1,7 +1,4 @@
 def missing_number(n, numbers):
-    return (n*(n+1)//2) - sum(numbers)
+    return (n * (n + 1) // 2 ) - sum(numbers)
 
-
-
-# assert missing_number(5, [1, 2, 3, 4, 5]) == 3, "Error en el caso de prueba"
-print(missing_number(6, [1, 2, 3, 4, 6]))
+print(missing_number(9, [1, 2, 3, 4, 5, 6, 7, 9]))
